69acc6e62e59f59ab59e0803b494ffb528028123
sawada <sawada@csplan.jp>
2021-10-03 11:21:33 +0900

    add help option

--- app/app.go
+++ app/app.go
@@ -11,19 +11,22 @@ import (
 	"github.com/mattn/go-pipeline"
 )
 
-
-var filesPath string = os.Getenv("HOME")+"/.cli_memo_app/memos/"
+var filesPath string = os.Getenv("HOME") + "/.cli_memo_app/memos/"
 
 func Run() {
 	if len(os.Args) == 1 {
 		// no args
 		fmt.Println(stdOut(callFZF()))
 		return
-	} 
+	}
 
 	switch os.Args[1] {
 	case "-c", "--create":
 		createMemo(new())
+	case "-h", "--help":
+		showHelp()
+	default:
+		fmt.Println(os.Args[1], "は存在しないオプションです。")
 	}
 }
 
@@ -70,11 +73,18 @@ func createMemo(contents map[string]string) {
 	fp.WriteString(contents["body"])
 }
 
+func showHelp() {
+	fmt.Println("cmemo <option>")
+	fmt.Println("")
+	fmt.Println("オプション無し : 既存のメモ一覧を表示します。")
+	fmt.Println("-c, --create   : memoを作成します。")
+	fmt.Println("-h, --help     : helpを表示します。")
+}
 
 func new() map[string]string {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	contents := map[string]string {
+	contents := map[string]string{
 		"title": "",
 		"body": "",
 	}
