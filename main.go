79f6a2c3610bb11e32b0e3f5f0a349ddd9231505
sawada <sawada@csplan.jp>
2021-12-07 17:15:20 +0900

    add fzf check

--- main.go
+++ main.go
@@ -3,12 +3,25 @@ package main
 import (
 	"cliMemoApp/app"
 	"fmt"
+	"os/exec"
 )
 
 func main() {
 	fmt.Println("\x1b[31m+---------------------------------------------------------+\x1b[0m")
 
-	app.Run()
+	if existFZF() {
+		app.Run()
+	} else {
+		fmt.Println("fzf が存在しません。")
+		fmt.Println("ダウンロードして下さい。")
+		fmt.Println("GitHubリポジトリ：https://github.com/junegunn/fzf")
+	}
 
 	fmt.Println("\x1b[31m+---------------------------------------------------------+\x1b[0m")
+}
+
+func existFZF() bool {
+	output, _ := exec.Command("which", "fzfff").Output()
+
+	return len(output) != 0
 }
